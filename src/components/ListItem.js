ae7c0b316de928615b620dbf2b04e8486bcceba7
ShehanRajapaksha <shehan.rajapaksha2002@gmail.com>
2023-07-20 20:39:18 +0530

    updated Current and Upcoming weather

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,50 +1,94 @@
-/* eslint-disable react/prop-types */
-import React from "react"
-import {View, Text, StyleSheet} from 'react-native'
-import {Feather} from '@expo/vector-icons'
-import { WeatherType } from "../utilities/WeatherType"
-import moment from "moment"
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { WeatherType } from "../utilities/WeatherType";
+import moment from "moment";
 
+const ListItem = (props) => {
+  const { dt_txt, min, max, condition, tempnow, isMainItem } = props;
+  const { date, item, tempContainer, tempNow, tempMinMax, iconContainer } = styles;
 
-
-const ListItem = (props) =>{
-    const {dt_txt,min,max,condition}=props
-    const { date,item,temp}=styles
-    return(
-        <View style={item}>
-            <Feather name={WeatherType[condition]?.icon } size={50} color={'white'}/>
-            <View style={styles.dateTextWrapper}>
-                <Text style={date}>{moment(dt_txt).format('dddd')}</Text>
-                <Text style={date}>{moment(dt_txt).format('h:mm:ss a')}</Text>
-            </View>
-            <Text style={temp}>{`${Math.round(min)}° /${Math.round(max)}°`}</Text>
-            
+  if (isMainItem) {
+    return (
+      <View style={styles.firstItem}>
+        <View style={iconContainer}>
+        <Feather name={WeatherType[condition]?.icon} size={80} color={"white"} />
         </View>
+        <View style={styles.firstTempContainer}>
+        <Text style={tempNow}>{`${tempnow.toFixed(1)}°`}</Text>
+        <Text style={tempMinMax}>{`${Math.round(min)}° / ${Math.round(max)}°`}</Text>
+      </View>
+        <Text>True</Text>
+      </View>
+    );
+  }
 
-    )
-}
+  return (
+    <View style={item}>
+      <View style={iconContainer}>
+        <Feather name={WeatherType[condition]?.icon} size={50} color={"white"} />
+      </View>
+      <View style={styles.dateTextWrapper}>
+        <Text style={date}>{moment(dt_txt).format("dddd")}</Text>
+        <Text style={date}>{moment(dt_txt).format("h:mm:ss a")}</Text>
+      </View>
+      <View style={tempContainer}>
+        <Text style={tempNow}>{`${tempnow.toFixed(1)}°`}</Text>
+        <Text style={tempMinMax}>{`${Math.round(min)}° / ${Math.round(max)}°`}</Text>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    item:{
-        padding:20,
-        marginVertical:8,
-        marginHorizontal:16,
-        flexDirection:'row',
-        justifyContent:'space-around',
-        alignItems:'center',
-        borderWidth:5,
-        backgroundColor:'indianred'
-    },
-    temp:{
-        color:'white',
-        fontSize:20
-    },
-    date:{
-        color:'white',
-        fontSize:15 
-    },
-    dateTextWrapper:{
-        flexDirection:'column'
-    }
-})
-export default ListItem
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "white",
+  },
+  mainItem: {
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  tempContainer: {
+    alignItems: "flex-end",
+    marginLeft: 120,
+  },
+  tempNow: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  tempMinMax: {
+    color: "white",
+    fontSize: 16,
+  },
+  date: {
+    color: "white",
+    fontSize: 15,
+  },
+  dateTextWrapper: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+  },
+  iconContainer: {
+    marginRight: 16,
+  },
+  firstItem:{
+    flex:1,
+    padding:30,
+    flexDirection:'column',
+    alignItems:'center',
+    justifyContent:'flex-start'
+  },
+  firstTempContainer:{
+    padding:10,
+  }
+});
+
+export default ListItem;
--- src/screens/UpComingWeather.js
+++ src/screens/UpComingWeather.js
@@ -1,44 +1,63 @@
-import React from "react"
-import { SafeAreaView , StyleSheet ,  FlatList,  StatusBar, ImageBackground} from "react-native"
+import React from "react";
+import { SafeAreaView, StyleSheet, FlatList, StatusBar } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import ListItem from "../components/ListItem";
 
-import ListItem from "../components/ListItem"
-
-
-
-const UpComingWeather = ({weatherData}) => {
-    const renderItem = ({item}) =>(
-        <ListItem 
-        condition ={item.weather[0].main} 
-        dt_txt={item.dt_txt} 
-        min={item.main.temp_min} 
-        max={item.main.temp_max} />
-    )
-    const{container,image}= styles
-    return(
-        <SafeAreaView style={container}>
-            <ImageBackground source={require('../../assets/upcoming-background.jpg')}
-             style={image}
-            >
-          
-            <FlatList
-                data={weatherData}
-                renderItem={renderItem}
-                keyExtractor={(item)=>item.dt_txt}
-                />
-        </ImageBackground>   
-        </SafeAreaView>
-
- )
-}
-const styles  = StyleSheet.create({
-    container:{
-        flex:1,
-        marginTop:StatusBar.currentHeight ||  0,
-        backgroundColor:'royalblue'
-    },
-   
-    image:{
-        flex:1
+const UpComingWeather = ({ weatherData }) => {
+  const renderItem = ({ item, index }) => {
+    // Check if it's the first item in the list
+    if (index === 0) {
+      return (
+        <ListItem
+          condition={item.weather[0].main}
+          dt_txt={item.dt_txt}
+          tempnow={item.main.temp}
+          min={item.main.temp_min} 
+          max={item.main.temp_max}
+          isMainItem // Add a custom prop to identify the main item
+        />
+      );
+    } else {
+      return (
+        <ListItem
+          condition={item.weather[0].main}
+          dt_txt={item.dt_txt}
+          min={item.main.temp_min}
+          max={item.main.temp_max}
+          tempnow={item.main.temp}
+        />
+      );
     }
-}) 
-export default UpComingWeather
+  };
+
+  return (
+    <LinearGradient
+      colors={["#5B8BDF", "#00CCE5"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.linearGradient}
+    >
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={weatherData}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.dt_txt}
+        />
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  linearGradient: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default UpComingWeather;
